7f12d571086e8c0ce95d344e974757dfa34063dd
Ernest <zyperis14@gmail.com>
2025-09-09 15:00:17 +0300

    WIP

---
+++ src/elements/Create.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import {Link, useNavigate} from 'react-router-dom';
+
+export function Create() {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        age: '',
+        gender: '',
+    });
+
+    const navigate = useNavigate();
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        axios.post('/add_user', values)
+        .then((res) => {
+            navigate('/');
+            console.log(res);
+        })
+        .catch((err) => console.log(err))
+    };
+
+    return (
+        <div className="container vh-100 vw-100 bg-primary">
+            <div className="row">
+                <h3>Add Student</h3>
+                <div className="d-flex justify-content-end">
+                    <Link to='/' className='btn btn-success'>Home</Link>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" onChange={e => setValues({...values, name: e.target.value})} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" onChange={e => setValues({...values, email: e.target.value})} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="gender">Gender</label>
+                        <input type="text" name="gender" onChange={e => setValues({...values, gender: e.target.value})} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="age">Age</label>
+                        <input type="text" name="age" onChange={e => setValues({...values, age: e.target.value})} />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+};
