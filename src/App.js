92a572ce751ebb9bd3242d6325bf07f6fd5666a4
Ernest <zyperis14@gmail.com>
2025-09-10 10:17:13 +0300

    comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './elements/Home';
+import React from "react"; //Importuojam React biblioteką
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; //Iš react-router-dom bibliotekas paimame nurodytus elementus
+import 'bootstrap/dist/css/bootstrap.min.css'; //Įkeliame bootstrap stylių klases
+import Home from './elements/Home'; //Įkeliame mūsų sukurtus elementus
 import Create from './elements/Create';
 import Edit from './elements/Edit';
 import Read from './elements/Read';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
+function App() { //Sukuriame funciją
+  return ( //Nurodome ką grąžina funkcija
+    <BrowserRouter> {/* Apgaubia visą aplikaciją ir leidžia naudoti routing sistemą. Stebi URL ir pagal adresą nurodo, kurį komponentą rodyti */}
+      <Routes> {/* Apgaubia tik viduje nurodytus elementus. Parodo tinkamą komponentą pagal adresą */}
+        <Route path='/' element={<Home />} /> {/* Nurodo, kad reikia rodyti 'Home' elementą, esant '/' */}
         <Route path='/create' element={<Create />} />
-        <Route path='/edit/:id' element={<Edit />} />
+        <Route path='/edit/:id' element={<Edit />} /> {/* ':id' reiškia dinaminį parametrą URL, pvz. /edit/5 redaguos studentą su id=5 */}
         <Route path='/read/:id' element={<Read />} />
       </Routes>
     </BrowserRouter>
   )
 };
 
-export default App;
+export default App; //Exportuojam 'App' funkciją, kad kiti failai galetų naudoti šią funkciją
